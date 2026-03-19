0c7f05560db6913ead39b5f2264c1702561c93b0
TogiraIkonoka1 <antonmcassidy@gmail.com>
2026-03-19 10:49:51 -0400

    Added loading logic for gitHub pages

--- js/main.js
+++ js/main.js
@@ -1,13 +1,45 @@
 
 let bubbleChart;
 
-d3.json("data/filtered_sample1.json").then(function(data) {
-    bubbleChart = new BubbleChart("#chart-area", data);
+function loadDataWithFallback() {
+    const pageDir = window.location.pathname.replace(/\/[^/]*$/, "/");
+    const candidates = [
+        "data/filtered_sample1.json",
+        "./data/filtered_sample1.json",
+        `${pageDir}data/filtered_sample1.json`
+    ];
 
-    const categoryDropdown = d3.select("#categorySelection");
-    bubbleChart.setCategory(categoryDropdown.property("value"));
+    let lastError = null;
 
-    categoryDropdown.on("change", function() {
-        bubbleChart.setCategory(this.value);
+    return candidates.reduce((promise, url) => {
+        return promise.catch(() => {
+            return d3.json(url).catch(err => {
+                lastError = err;
+                throw err;
+            });
+        });
+    }, Promise.reject(new Error("No URL attempted yet"))).catch(() => {
+        throw lastError || new Error("Failed to load dataset");
     });
-});
+}
+
+loadDataWithFallback()
+    .then(function(data) {
+        bubbleChart = new BubbleChart("#chart-area", data);
+
+        const categoryDropdown = d3.select("#categorySelection");
+        bubbleChart.setCategory(categoryDropdown.property("value"));
+
+        categoryDropdown.on("change", function() {
+            bubbleChart.setCategory(this.value);
+        });
+    })
+    .catch(function(error) {
+        console.error("Dataset loading failed:", error);
+
+        d3.select("#chart-area")
+            .append("p")
+            .style("color", "#b00020")
+            .style("font-weight", "600")
+            .text("Could not load chart data. Verify the data file path for GitHub Pages.");
+    });
